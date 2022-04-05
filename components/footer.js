d1c13a58dc3855d55b96b267f03a15b1b9563baf
Luke Novak <contact@nluken.com>
2022-04-05 01:56:26 -0400

    footer adjustments

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,9 @@ export default function Footer({pages, address}) {
         <div className="blue-bg">
             <div className="container text-center justify-content-center">
                 <div className="row">
-                    <div className="col-4">{address}</div>
+                    <div className="col-4">
+                        <span className="align-self-center">{address}</span>
+                        </div>
                     <div className="col-8">
                         {pages.map((page) => <PageNav {...page}/>)}
                     </div>
