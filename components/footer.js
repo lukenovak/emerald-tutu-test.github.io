e4692ec81abcaa914ba3a08a14aa509bd5b399bf
Luke Novak <contact@nluken.com>
2022-04-06 00:56:24 -0400

    sections in footers

--- components/footer.js
+++ components/footer.js
@@ -3,14 +3,16 @@ import PageNav from "./page-nav"
 export default function Footer({pages, address}) {
     return(
         <div className="blue-bg">
-            <div className="container text-center justify-content-center">
+            <div className="text-center justify-content-center">
                 <div className="row">
                     <div className="col-4">
                         <span className="align-self-center">{address}</span>
                         </div>
-                    <div className="col-8">
-                        {pages.map((page) => <PageNav {...page}/>)}
-                    </div>
+                        {pages.map((page) => 
+                            <div className="col-2">
+                                <PageNav {...page}/>
+                            </div>
+                        )}
                 </div>
             </div>
         </div>
