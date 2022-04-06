643fcd01e7321e342574178b3ab17ad35cdd1665
Luke Novak <contact@nluken.com>
2022-04-06 02:15:35 -0400

    fixed pictures + text size

--- components/blocks/text.js
+++ components/blocks/text.js
@@ -1,8 +1,9 @@
-export default function Text({text}) {
+export default function Text({text, isBlueBackground}) {
     return (
-        <div className="container">
+        <div className="container text">
             {text.map((paragraph, idx) => {
                 var paragraphClasses = "col-12 col-sm-10 col-md-8"
+                paragraphClasses = isBlueBackground ? paragraphClasses + " blue-bg" : paragraphClasses
                 return (
                 <div className="row justify-content-center">
                     <p key={idx} className={paragraphClasses}>{paragraph}</p>
