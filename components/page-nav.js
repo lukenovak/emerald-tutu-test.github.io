1940e615d58f2f7d361aafe579ae4767e6fc7e80
Luke Novak <contact@nluken.com>
2022-04-06 00:56:24 -0400

    sections in footers

--- components/page-nav.js
+++ components/page-nav.js
@@ -1,9 +1,17 @@
 import circleNum from "./utils"
 
-export default function PageNav({pageNumber, title, slug}){
+export default function PageNav({pageNumber, title, slug, blocks}){
     return(
-        <div className="text-center font-weight-bold hidden-link">
-            <a href={slug}>{circleNum(pageNumber)} {title}</a>
-        </div>
+        <div className="hidden-link">
+            <div className="d-inline font-weight-bold hidden-link">
+                <a className="font-monument" href={slug}>{circleNum(pageNumber)} {title}</a>
+            </div>
+            {blocks && blocks.map((section) => 
+                <div className="pl-2">
+                    <a href={slug + "#" + section.name}>{section.name}</a>
+                </div>)}
+            </div>
+       
+
     )
 }
