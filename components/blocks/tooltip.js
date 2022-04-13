00e6335f66946dfa50f632c34189fe583462afab
Luke Novak <contact@nluken.com>
2022-04-12 23:19:32 -0400

    added tooltip

--- components/blocks/tooltip.js
+++ components/blocks/tooltip.js
@@ -6,11 +6,11 @@ export default function Tooltip({xPos, yPos, number, text}) {
     }
     return (
         <div 
-            className="position-absolute blue-bg text-bold"
+            className="position-absolute d-flex blue-bg text-bold p-2 align-middle annotation"
             style={positionStyles}>
-            <div className="d-flex align-content-middle">{"" + number}</div>
-            <div className="invisible">
-                <p>{text}</p>
+            <div className="h2 my-0 align-content-middle">{"" + number}</div>
+            <div>
+                <p className="ms-3 my-auto annotation-text">{text}</p>
             </div>
         </div>
     )
