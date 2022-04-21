c96d186b38bc7578ffb89ebc2e9274aa9dbaa277
Luke Novak <contact@nluken.com>
2022-04-20 21:06:42 -0400

    fixes to people component

--- components/blocks/person.js
+++ components/blocks/person.js
@@ -3,22 +3,27 @@ import Image from 'next/image'
 export default function Person({name, title, bio, photo}) {
     console.log(photo)
     return (
-        <div className="row">
-            {photo && (
-                <div className="col-4 blue-border">
-                    <Image 
-                        src={photo.url}
-                        height={photo.height}
-                        width={photo.width}
-                    />
+        <div className="container p-0 my-3">
+            <div className="row mb-3">
+                {photo && (
+                    <div className="col-4 p-0 blue-border d-flex">
+                        <Image 
+                            src={photo.url}
+                            height={photo.height}
+                            width={photo.width}
+                        />
+                    </div>
+                )}
+                <div className="col-8 my-auto">
+                    <h4>{name}</h4>
+                    <h5>{title}</h5>
                 </div>
-            )}
-            <div className="col-8">
-                <h4>{name}</h4>
-                <h5>{title}</h5>
-                <p>{bio}</p>
+            </div>
+            <div className='row'>
+                <p className="p-0">{bio}</p>
             </div>
         </div>
+        
     )
 
 }
