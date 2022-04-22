4712e090f040bb0b7a931d67e9c6d2a8332efa48
Luke Novak <contact@nluken.com>
2022-04-21 23:15:23 -0400

    fixed main menu + embeddables

--- components/blocks/big-menu.js
+++ components/blocks/big-menu.js
@@ -1,12 +1,40 @@
-import PageNav from "../page-nav"
-import { ONLY_SHOW_ON_CLICK } from "../page-nav"
+import { Collapse } from "react-bootstrap"
+import circleNum from "../utils"
+import { useState } from "react";
 
 export default function BigMenu({pages}) {
     return(
-        <div>
-            <ul className="h1">
-                {pages.map((page) => <PageNav {...page} mode={ONLY_SHOW_ON_CLICK}/>)}
-            </ul>
+        <div className="container mx-auto h1 hidden-link">
+            {pages.map((page) => {
+                const [open, setOpen] = useState(false);
+                // This should be a PageNav but we need some more custom code for the collapsing
+                return (
+                    <div 
+                        id={"page" + page.title} 
+                        className="row"
+                        onMouseEnter={() => setOpen(!open)}
+                        onMouseLeave={() => setOpen(!open)}
+                        aria-expanded={open}
+                    >
+                        <a href={"/" + page.slug} className="display-1 font-monument hidden-link text-center">
+                            <div className="float-start me-3">
+                                {circleNum(page.pageNumber)}
+                            </div>
+                            <div className="float-start">
+                                {page.title}
+                            </div>
+                        </a>
+                        <Collapse in={open}>
+                        <div id={"pagenav-" + page.pageNumber + "-blocks"}>
+                            {page.blocks.map((section) => 
+                                <div className="pl-2">
+                                    <a href={page.slug + "#" + section.name}>{section.name}</a>
+                                </div>)}
+                        </div>
+                        </Collapse>                        
+                    </div>
+                )
+            })}
         </div>
     )
 }
