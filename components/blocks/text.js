77dd02f38e1c9fea9a1f8505491b7a733488debd
Luke Novak <contact@nluken.com>
2022-04-28 23:19:00 -0400

    small fixes

--- components/blocks/text.js
+++ components/blocks/text.js
@@ -1,12 +1,10 @@
 export default function Text({text, isBlueBackground}) {
     return (
-        <div className="container text">
+        <div className="container text py-2 w-auto">
             {text.map((paragraph, idx) => {
-                var paragraphClasses = "col-12 col-sm-10 col-md-8"
-                paragraphClasses = isBlueBackground ? paragraphClasses + " blue-bg" : paragraphClasses
                 return (
                 <div className="row justify-content-center">
-                    <p key={idx} className={paragraphClasses}>{paragraph}</p>
+                    <p key={idx} className={"col-12 col-sm-10 col-md-8 mb-0 py-2" + (isBlueBackground ? " blue-bg" : "")}>{paragraph}</p>
                 </div>
                 )
             })}
