204b63df3ae005514f23b01163c0461892f1a293
Luke Novak <contact@nluken.com>
2022-07-07 23:27:53 -0400

    fix styling

--- components/blocks/image-carousel.js
+++ components/blocks/image-carousel.js
@@ -14,7 +14,7 @@ export default function ImageCarousel({images, id, maxWidth}) {
     };
 
     return (
-        <div id={id} className="container row">
+        <div id={id} className="container row width-max-content m-auto">
             <div className="position-relative d-flex m-auto">
                 <div className="carousel-arrow left-arrow" onClick={prevSlide}>&lt;</div>
                 <div className="carousel-arrow right-arrow" onClick={nextSlide}>&gt;</div>
