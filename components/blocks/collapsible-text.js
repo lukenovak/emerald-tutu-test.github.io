8cef77d69e8e32989fc8d45d1729f231b7427fdf
Luke Novak <contact@nluken.com>
2022-11-28 21:58:53 -0500

    collapsibles open by default

--- components/blocks/collapsible-text.js
+++ components/blocks/collapsible-text.js
@@ -2,18 +2,18 @@ import { useState } from "react"
 import { Collapse } from "react-bootstrap"
 
 export default function CollapsibleText({heading, text, highlight}) {
-    var [isOpen, setOpen] = useState()
+    var [isExpanded, setExpanded] = useState({isExpanded: true})
     return (
         <div className={"container col-12 col-md-10 col-xl-8 p-2 my-2" + (highlight? " blue-bg" : "")}>
-            <div className="row" onClick={() => setOpen(!isOpen)}>
+            <div className="row" onClick={() => setExpanded(!isExpanded)}>
                 <div className="d-inline-flex">
                     <h4 className="mb-0">{heading}</h4>
                     <span className="ms-auto">
-                        {isOpen ? "-" : "+"}
+                        {isExpanded ? "-" : "+"}
                     </span>
                 </div>
             </div>
-            <Collapse in={isOpen}>
+            <Collapse in={isExpanded}>
                 <p className="mt-2 mb-0">{text}</p>
             </Collapse>
         </div>
