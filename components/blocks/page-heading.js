d187e684d092a0b61b0d5d054ab0c3aa346d9ad7
Luke Novak <contact@nluken.com>
2023-01-14 16:51:13 -0500

    improved header

--- components/blocks/page-heading.js
+++ components/blocks/page-heading.js
@@ -2,7 +2,7 @@ import circleNum from "../utils"
 
 export default function PageHeading({page}) {
     console.log(page)
-    return <div className="col-12 display-2 font-monument hidden-link w-auto text-center d-flex">
+    return <div id="pheading" className="col-12 display-2 font-monument hidden-link w-auto text-center d-flex">
             <div className="m-auto d-flex">
                 <div className="me-3">
                     {circleNum(page.pageNumber)}
--- components/header.js
+++ components/header.js
@@ -2,16 +2,44 @@ import circleNum from "./utils";
 import Link from "next/link";
 import SiteLogo from "./site-logo";
 
+import { useState, useEffect } from "react";
+
 // takes a second, blank arg because we give it the footer information
 export default function Header({pages, _}) {
-    return <div className="text-center bg-green header font-monument hidden-link w-100 position-fixed d-flex px-5">
+
+    const [scrolling, setScrolling] = useState(false);
+    const [scrollTop, setScrollTop] = useState(0);
+
+    const onScroll = (e) => {
+        setScrollTop(e.target.documentElement.scrollTop);
+        setScrolling(e.target.documentElement.scrollTop > scrollTop);
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', onScroll);
+    },[]);
+
+    useEffect(() => {
+    console.log(scrollTop);
+    }, [scrollTop])
+    
+    const baseClass = "text-center bg-green header font-monument hidden-link w-100 position-fixed d-flex mx-auto px-5"
+
+    return <div className=
+        {scrollTop > 100 ? baseClass : baseClass + " d-none"}>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <Link href="/">
-            <SiteLogo text="The Emerald Tutu"/>
+            <SiteLogo text="Emerald Tutu"/>
         </Link>
         {pages?.map((page, _) => 
-            <div className="d-inline mx-5 font-weight-bold hidden-link">
+            <div className="d-inline mx-auto font-weight-bold hidden-link">
                 <a className="font-monument" href={page.slug}>{circleNum(page.pageNumber)} {page.title}</a>
             </div>
         )}
+        <div class="d-flex" onClick={()=> window.scrollTo({top: 0})}>
+            <span class="material-symbols-outlined my-auto text-bold">
+            arrow_upward
+            </span>
+        </div>
     </div>
 }
