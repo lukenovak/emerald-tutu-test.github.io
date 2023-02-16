4ec50cdd3d4220463aa6a637db25b1017b22d82b
Luke Novak <contact@nluken.com>
2023-02-15 23:13:56 -0500

    cleaned up collapsible text a bit

--- components/blocks/collapsible-text.js
+++ components/blocks/collapsible-text.js
@@ -4,14 +4,12 @@ import { Collapse } from "react-bootstrap"
 export default function CollapsibleText({heading, text, highlight}) {
     var [isExpanded, setExpanded] = useState({isExpanded: false})
     return (
-        <div className={"container col-12 col-md-10 col-xl-8 p-2 my-2" + (highlight? " blue-bg" : "")}>
-            <div className="row" onClick={() => setExpanded(!isExpanded)}>
-                <div className="d-inline-flex">
-                    <h4 className="mb-0">{heading}</h4>
-                    <span className="ms-auto">
-                        {!isExpanded ? "-" : "+"}
-                    </span>
-                </div>
+        <div className={"container col-12 col-md-10 col-xl-8 p-2 my-2" + (highlight? " blue-bg" : "")} onClick={() => setExpanded(!isExpanded)}>
+            <div className="d-inline-flex w-100 px-1">
+                <h4 className="mb-0 w-100">{heading}</h4>
+                <span>
+                    {!isExpanded ? "-" : "+"}
+                </span>
             </div>
             <Collapse in={!isExpanded}>
                 <p className="p-1 mt-2 mb-0 fade-in">{text}</p>
